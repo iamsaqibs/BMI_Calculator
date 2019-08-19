e0a7d806d34032616e5b8f0816f30795a1b2a98e
Saqib S <msaqibsaeed4@gmail.com>
2019-08-19 21:35:57 +0500

    Added Slider

--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
-
 //Input Page Constants
 const activeReusableCardColor = Color(0xFF1D1E33);
 const inActiveReusableCardColor = Color(0xFF111328);
@@ -10,9 +9,13 @@ const maleIcon = FontAwesomeIcons.mars;
 const femaleIcon = FontAwesomeIcons.venus;
 const String male = 'Male';
 const String female = 'Female';
+const heightTextStyle = TextStyle(
+  fontSize: 30,
+  fontWeight: FontWeight.w900,
+);
 
 //Icon Content Constants
 const textStyle = TextStyle(
   color: Colors.white,
-  fontSize: 18.0,
+  fontSize: 12.0,
 );
--- lib/input_page.dart
+++ lib/input_page.dart
@@ -11,6 +11,7 @@ class InputPage extends StatefulWidget {
 class _InputPageState extends State<InputPage> {
   Color maleActiveColor = inActiveReusableCardColor;
   Color femaleActiveColor = activeReusableCardColor;
+  int height = 180;
 
   @override
   Widget build(BuildContext context) {
@@ -20,6 +21,7 @@ class _InputPageState extends State<InputPage> {
         title: Text('BMI CALCULATOR'),
       ),
       body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
         children: <Widget>[
           //First Row
           Expanded(
@@ -68,7 +70,46 @@ class _InputPageState extends State<InputPage> {
 
           //Second Row Starting
           Expanded(
-            child: ReusableCard(color: activeReusableCardColor),
+            child: ReusableCard(
+              color: activeReusableCardColor,
+              cardChild: Column(
+                // crossAxisAlignment: CrossAxisAlignment.stretch,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: <Widget>[
+                  Text(
+                    'HEIGHT',
+                    style: textStyle,
+                  ),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.baseline,
+                    textBaseline: TextBaseline.alphabetic,
+                    children: <Widget>[
+                      Text(
+                        height.toString(),
+                        style: heightTextStyle,
+                      ),
+                      Text(
+                        'cm',
+                        style: textStyle,
+                      ),
+                    ],
+                  ),
+                  Slider(
+                    value: height.toDouble(),
+                    min: 120.0,
+                    max: 220,
+                    activeColor: Color(0xFFEA1556),
+                    inactiveColor: Color(0xFF8D8E98),
+                    onChanged: (double newHeight){
+                      setState(() {
+                        height = newHeight.toInt();
+                      });
+                    },
+                  )
+                ],
+              ),
+            ),
           ),
           //Second Row Ending
 
