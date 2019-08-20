7360dee5f08278f45d85412ae4a043085ea9feca
Saqib S <msaqibsaeed4@gmail.com>
2019-08-20 20:13:34 +0500

    Rounded Icon Button added and changes in result page.

---
+++ lib/resultsPage.dart
@@ -0,0 +1,20 @@
+import 'package:bmi_calculator/reusableCard.dart';
+import 'package:flutter/material.dart';
+import 'constants.dart';
+
+class ResultsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('BMI Calculator'),
+      ),
+      body: Column(
+        children: <Widget>[
+          
+        ],
+      ),
+      backgroundColor: primaryColor,
+    );
+  }
+}
